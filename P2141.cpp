151b50439c048d70d7fb424363493d9525d2dd41
miqianmimi <miqianmimi@outlook.com>
2018-07-04 11:04:38 +0800

    2018.7.4

--- P2141.cpp
+++ P2141.cpp
@@ -1,18 +1,21 @@
 #include<cstring>
+#include<cstdio>
+#include<cstdlib>
 #include<iostream>
+#include<algorithm>
 int n ;
 int num[101];
-int ans ;
+int ans = 0 ;
 int flag[100000];
 int main(){
 	scanf("%d",&n);
 	memset(flag,0,sizeof(flag));	
 	for (int i=0;i<n;i++){
 		scanf("%d",&num[i]);
-		flag[i] = 1;
+		flag[num[i]] = 1;
 	}
 	for (int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
+		for(int j=0;j<i;j++){
 			if (flag[num[i]+num[j]]){
 				flag[num[i]+num[j]]=0;
 				ans++;
