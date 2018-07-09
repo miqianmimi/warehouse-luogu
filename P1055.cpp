e1971effc4d9b409e99fda7395bb0fdde758070b
miqianmimi <miqianmimi@outlook.com>
2018-07-09 23:09:07 +0800

    7-11

--- P1055.cpp
+++ P1055.cpp
@@ -6,18 +6,20 @@
 using namespace std;
 int main(){
 	char a[14] ;
-	int b = 0 ;
+	char b ;
+	char t ;
 	int sum = 0 ;
-	scanf("%c-%c%c%c-%c%c%c%c%c-%d",&a[0],&a[1],&a[2],&a[3],&a[4],&a[5],&a[6],&a[7],&a[8],&b);
+	scanf("%c-%c%c%c-%c%c%c%c%c-%c",&a[0],&a[1],&a[2],&a[3],&a[4],&a[5],&a[6],&a[7],&a[8],&b);
 	for( int i = 1 ; i <= 9 ; i ++ ){
 		sum += (a[i - 1] - '0') * i;
 	}
-	if ( sum % 11 == b ){
+	t = sum%11 + '0';
+	if (t =='0'+10) t = 'X';
+	if ( t == b ){
 		printf("Right\n");
 	} 
 	else {
-		int right = sum % 11;
-		printf("%c-%c%c%c-%c%c%c%c%c-%d\n",a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],right);
+		printf("%c-%c%c%c-%c%c%c%c%c-%c\n",a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],t);
 	}
 	return 0 ;
 }
